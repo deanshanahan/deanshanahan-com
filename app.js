a9ee08861d7aa1b50abcfc74434bb9178f59b70f
Dean Shanahan <dean.shanahan@gmail.com>
2019-12-04 21:45:08 +0100

    Add favicon and styling

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const favicon = require('serve-favicon')
+const path = require('path')
 
 const home = require('./home');
 
 const app = express();
+
 
 app.use(cors());
 
@@ -15,5 +18,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('', home);
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')))
+app.use(express.static(__dirname + '/public'));
 
 module.exports = app;
